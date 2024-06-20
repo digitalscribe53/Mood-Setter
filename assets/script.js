1fab71c4fd6ba14e55ae68f7d1865ed8c8654268
LeonathaTurtle <mancillh@gmail.com>
2024-06-20 17:01:08 -0500

    added logic to allow gifs found in GIPHY API to display on screen, no form interaction at the moment

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
-//const GIPHYAPIKey = ATZHOCugJY4c84N9kSARyOvLB2gL4xi6
+const GIPHYAPIKey = "ATZHOCugJY4c84N9kSARyOvLB2gL4xi6";
+const userInterests = document.querySelector("#UserInterests"); //change id after querySelector to match form
+
+//returns 5 GIFs in repsonse to keywords from the user's interests field in the form
+//const GIFQueryURL = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHYAPIKey}&q=${userInterests}&limit=5&offset=0&rating=r&lang=en&bundle=messaging_non_clips`;
+const GIFQueryURL = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHYAPIKey}&q=cats&limit=5&offset=0&rating=r&lang=en&bundle=messaging_non_clips`;
+
+function fetchGifData() {
+    fetch(GIFQueryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (GIFdata) {
+            console.log(GIFdata);
+            for (i = 0; i < GIFdata.data.length; i++) {
+                let figure = document.createElement('figure');
+                let image = document.createElement('img');
+                let caption = document.createElement('figcaption');
+                image.src = GIFdata.data[i].images.fixed_height.url;
+                image.alt = GIFdata.data[i].title;
+                caption.textContent = GIFdata.data[i].title;
+                figure.append(image, caption);
+                let gifDisplay = document.querySelector('#gifDisplay')
+                gifDisplay.append(figure);
+            }
+        })
+};
+
+fetchGifData();
