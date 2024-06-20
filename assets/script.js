d6ccd1a1f62f6b31d11b8b31fdac0d2daad5104b
LeonathaTurtle <mancillh@gmail.com>
2024-06-20 18:54:18 -0500

    forced gifs into box

--- assets/script.js
+++ assets/script.js
@@ -11,18 +11,19 @@ function fetchGifData() {
             return response.json();
         })
         .then(function (GIFdata) {
-            console.log(GIFdata);
+            
             for (i = 0; i < GIFdata.data.length; i++) {
                 let figure = document.createElement('figure');
                 let image = document.createElement('img');
                 let caption = document.createElement('figcaption');
                 image.src = GIFdata.data[i].images.fixed_height.url;
                 image.alt = GIFdata.data[i].title;
-                caption.textContent = GIFdata.data[i].title;
-                figure.append(image, caption);
-                let gifDisplay = document.querySelector('#gifDisplay')
+                //caption.textContent = GIFdata.data[i].title;
+                figure.append(image);
+                let gifDisplay = document.querySelector('#gif-box')
                 gifDisplay.append(figure);
             }
+
         })
 };
 
