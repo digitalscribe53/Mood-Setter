9096eafb9523bb854a506a4b09890d0fabd153aa
emangelic <ema.angelic@outlook.com>
2024-06-24 15:20:18 -0400

    added theme logic, added  license, and updated readme

--- assets/script.js
+++ assets/script.js
@@ -52,8 +52,44 @@ document.addEventListener('DOMContentLoaded', () => {
     saveUserButton.addEventListener('click', (event) => {
         event.preventDefault();
         saveUserData();
+        setTheme();
     });
 
     // Event listener for "My Mood" button to open the modal
     myMoodButton.addEventListener('click', openModal);
 });
+
+function setTheme() {
+    // pulling the most recent user from local storage
+    var savedUserData = JSON.parse(localStorage.getItem('userDataArray'));
+    var lastSavedUser = savedUserData[savedUserData.length-1] // This will pull the most recently added user in the localstorage
+    var mood = lastSavedUser.mood;
+    console.log(mood);
+    switch (mood.toUpperCase()) {
+        case 'HAPPY' :
+            document.querySelector('#outer-box').classList.add('happy');
+            break;
+        case 'SAD' :
+            document.querySelector('#outer-box').classList.add('sad');
+            break;
+        case 'ANGRY' :
+            document.querySelector('#outer-box').classList.add('angry');
+            break;
+        case 'CALM' :
+            document.querySelector('#outer-box').classList.add('calm');
+            break;
+        case 'LOVE' :
+            document.querySelector('#outer-box').classList.add('love');
+            break;
+        case 'INSPIRED' :
+            document.querySelector('#outer-box').classList.add('inspired');
+            break;
+        case 'SPONTANEOUS' :
+            document.querySelector('#outer-box').classList.add('spontaneous');
+            break;
+        default:
+            document.querySelector('#outer-box').classList.add('default');
+    }
+    // take the mood key from the user object 
+    // build our switch case to handle their mood
+}
