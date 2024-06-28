2ac488de5c1f91d1f2cda81781bd3520c0858960
LeonathaTurtle <mancillh@gmail.com>
2024-06-27 20:52:44 -0500

    added hearts animation to JS and CSS

--- assets/script.js
+++ assets/script.js
@@ -170,19 +170,61 @@ function setTheme() {
             break;
         case 'CHILL' :
             document.querySelector('#outer-box').classList.add('chill');
-
             break;
         case 'LOVE':
             document.querySelector('#outer-box').classList.add('love');
+
+            // Special thanks to Developers Today
+            // website for heart animation: https://medium.com/@developerstoday99/create-animated-hearts-87b3271ae774
+            // Function that creates a heart element with random properties
+            function createHeart() {
+                const heart = document.createElement('div');
+                heart.classList.add('heart');
+                heart.style.width = `${Math.floor(Math.random() * 65) + 10}px`;
+                heart.style.height = heart.style.width;
+                heart.style.left = `${Math.floor(Math.random() * 100) + 1}%`;
+                heart.style.background = `rgba(255, ${Math.floor(Math.random() * 25) + 100 - 25}, ${Math.floor(Math.random() * 25) + 100}, 1)`;
+                const duration = Math.floor(Math.random() * 5) + 5;
+                heart.style.animation = `love2 ${duration}s ease`;
+                return heart;
+            }
+ 
+            // Get the container element where the hearts will be added
+            const container = document.querySelector('#outer-box');
+ 
+            // Function that removes hearts that have gone off screen
+            function removeHearts() {
+                const hearts = container.querySelectorAll('.heart');
+                hearts.forEach((heart) => {
+                const top = parseFloat(getComputedStyle(heart).getPropertyValue('top'));
+                const width = parseFloat(getComputedStyle(heart).getPropertyValue('width'));
+                if (top <= -100 || width >= 150) {
+                    heart.remove();
+                }
+                });
+            }
+ 
+            // Define a function that repeatedly adds hearts to the container
+            function addHeart() {
+                const heart1 = createHeart();
+                const heart2 = createHeart();
+                container.appendChild(heart1);
+                container.appendChild(heart2);
+                setTimeout(removeHearts, 1000);
+            }
+           
+            // Start the animation loop
+            const love2 = setInterval(addHeart, 500);
             break;
+            
         case 'INSPIRED':
             document.querySelector('#outer-box').classList.add('inspired');
             break;
 
         case 'HYPE' :
             document.querySelector('#outer-box').classList.add('hype');
-
             break;
+
         default:
             document.querySelector('#outer-box').classList.add('default');
     }
